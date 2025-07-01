8f654f89da35cdb14621da8cc74ca89cf680f8df
Balaji Manickavasagam <mebala147@gmail.com>
2025-07-01 23:03:25 +0530

    adding env to make ready for deloyment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios"
+const rootUrlBackend = process.env.REACT_APP_BASE_URL
 
 const API = axios.create({
-    baseURL:"http://localhost:4000/"
+    baseURL:rootUrlBackend
 })
 
 export const fetchPortfolioData = async () => {
