c6471dac56e352fd6bc83c3724fc58eedf4d4723
Balaji Manickavasagam <mebala147@gmail.com>
2025-07-02 14:29:56 +0530

    Production Ready Code

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const app = express()
 import dotenv from "dotenv"
 import { updatePortfolioData } from "./services/dataLoader.js"
 import fs from 'fs';
-const portfolio = JSON.parse(fs.readFileSync('./portfolio_data.json', 'utf-8'));
+
 
 dotenv.config()
 import cors from 'cors'
@@ -17,6 +17,7 @@ setInterval(async () => {
 }, 15000)
 
 app.get("/", async (req, res) => {
+  const portfolio = JSON.parse(fs.readFileSync('./portfolio_data.json', 'utf-8'));
   res.status(200).json({ "message": "Data fetched", portfolio: portfolio})
 })
 
